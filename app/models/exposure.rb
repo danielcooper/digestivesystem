6a3815fa819ee8b7345ca0a4be01ca0ddc4f609e
Kris Lipscombe <krislipscombe@gmail.com>
2010-08-26 16:43:54 +0100

    Merge

--- app/models/exposure.rb
+++ app/models/exposure.rb
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
-class Exposure < ActiveRecord::Base
-  belongs_to :stream
-  belongs_to :resource
-  accepts_nested_attributes_for :resource
-  has_one :service, :through => :stream
 
-  Resource.subclasses.each do |subclass|
-    belongs_to subclass.name.underscore.to_sym, :foreign_key => 'resource_id'
-    accepts_nested_attributes_for subclass.name.underscore.to_sym
-  end
- 
-  validates_presence_of :stream_id
-
-  def json_attributes
-    [:blurb,:title,:image,:name]
-  end
-
-  def to_json
-    resource.export_hash.merge(attributes.delete_if{|key,value| json_attributes.include?(key.to_sym) != true})
-  end
-
-  def admin_description
-    "TODO:IMPLEMENT ME!"
-  end
-
-end
-=======
 class Exposure < ActiveRecord::Base
   belongs_to :stream
   belongs_to :resource
@@ -59,4 +32,3 @@ class Exposure < ActiveRecord::Base
   end
 
 end
->>>>>>> b7cb6efed7657fef50e345bca8b5dc05ac7c303d
