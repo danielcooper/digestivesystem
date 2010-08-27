d687370c9bf3347c99867d1520afe613122047c9
Daniel Cooper <daniel@14lines.com>
2010-08-27 14:17:08 +0100

    Better creation of promotions, reasonable exposures index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def show_exposure_preview exposure
+    output = ""
+    output << %~<img id ="small_image" src="#{exposure.image}" width = '74' height = '74'/>~
+    if exposure.title
+      output << exposure.title
+    else
+      output << "No Title"
+    end
+    output << " <small>#{exposure.blurb}</small>" if exposure.blurb
+  end
 end
