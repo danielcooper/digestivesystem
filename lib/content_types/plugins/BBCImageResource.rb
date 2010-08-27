4958f450440a42d7a2ac1d7cc5cce19dd4089fb1
Daniel Cooper <daniel@14lines.com>
2010-08-27 10:48:54 +0100

    Added single image processing

---
+++ lib/content_types/plugins/BBCImageResource.rb
@@ -0,0 +1,27 @@
+class ContentTypes::Plugins::BBCImageResource < ContentTypes::Base
+
+  def initialize url
+    @url = url
+  end
+
+  def self.plugin_name
+    :image_resource
+  end
+
+  def self.model
+    "BbcImageResource"
+  end
+
+  def self.can_handle_resource_type? url
+    /https?:\/\/.*.(jpg|png|jpeg|gif|bmp|tiff)/.match(url)
+  end
+
+  def attributes
+    @attributes ||= begin
+      {:resource_url => @url, :type => self.class.model, :exturnal_image_url => @url}
+    end
+  end
+
+
+
+end
